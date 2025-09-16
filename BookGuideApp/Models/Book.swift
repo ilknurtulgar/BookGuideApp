90f07e3a5e5891531b38c360dfa6f7fd74928788
İlknur Tulgar <ilknur001-@hotmail.com>
2025-09-16 19:34:33 +0300

    Book model established

--- BookGuideApp/Models/Book.swift
+++ BookGuideApp/Models/Book.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
+struct BookResponse : Codable {
+    let items: [Book]?
+}
 
+struct Book: Codable, Identifiable {
+    let id: String
+    let volumeInfo: VolumeInfo
+}
+
+struct VolumeInfo: Codable {
+    let title: String?
+    let authors: [String]?
+    let publisher: String?
+    let imageLinks: ImageLinks?
+}
+
+struct ImageLinks: Codable {
+    let thumbnail: String?
+}
